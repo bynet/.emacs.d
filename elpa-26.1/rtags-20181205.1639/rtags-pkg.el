4ec0fd54e6cb61d43e4c70b27f04d57b807a793e
Cihangir Zengin <cihangirzengin@gmail.com>
2019-02-26 22:26:48 +0200

    cmake-ide is added

---
+++ elpa-26.1/rtags-20181205.1639/rtags-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rtags" "20181205.1639" "A front-end for rtags" 'nil :commit "40f5430c289a9232d7bc7b7407a0ee170b423466" :authors '(("Jan Erik Hanssen" . "jhanssen@gmail.com") ("Anders Bakken" . "agbakken@gmail.com")) :maintainer '("Jan Erik Hanssen" . "jhanssen@gmail.com") :url "http://rtags.net")
